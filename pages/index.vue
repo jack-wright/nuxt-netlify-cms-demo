6c730f497cfd1669d05219240213c3eacdc1e9c4
Jack Wright <jtwright89@gmail.com>
2018-07-26 16:05:01 +0100

    autoplaying video test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="container">
-       <h1>HOME</h1>
-       <a href="work">Work</a>
+        <h1>HOME</h1>
+        <a href="work">Work</a>
+        <video 
+            src="https://player.vimeo.com/external/268036290.hd.mp4?s=ed62fd973b78c6ac5ac525ce0378772e8149de9f&profile_id=175"
+            autoplay
+            muted
+            loop
+            webkit-playsinline
+            playsinline>
+        </video>
     </div>
 </template>
 
